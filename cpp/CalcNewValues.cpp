8328cc1fafa5d6489b141cc39525238480c60d22
bsolca <benjamin@solca.io>
2019-11-25 14:28:35 -0800

    [+] Rewrite algo

--- cpp/CalcNewValues.cpp
+++ cpp/CalcNewValues.cpp
@@ -4,8 +4,6 @@
 
 #include "CalcNewValues.hpp"
 
-#include <iostream>
-
 int main() {
     new CalcNewValues;
     return 0;
@@ -30,40 +28,46 @@ void CalcNewValues::printParam() {
 void CalcNewValues::readLines() {
     std::string inputLine;
 
-    do {
-        if (inputLine.length() > 0) {
-            if (inputLine.at(0) == '#') {
-                //This is a status line of text, and
-                //should be passed down the pipeline
-                std::cout << inputLine << std::endl;
-            } else if (inputLine.at(0) == '%') {
-                //This is an input burn rate
-                inputLine.erase(0);
-                std::cout << "on as@ " << inputLine << std::endl;
-                int burnRate = std::stoi(inputLine.erase(0));
+    while (std::getline(std::cin, inputLine) && altitude) {
+        std::cout << inputLine << std::endl;
+        if (!inputLine.rfind('#', 0)) {
+            //This is a status line of text, and
+            //should be passed down the pipeline
+            std::cout << inputLine << std::endl;
+        } else if (!inputLine.rfind('%', 0)) {
+            //This is an input burn rate
+            int burnRate = stringToInt(inputLine);
+            if (this->altitude <= 0) {
+                std::cout << "#The game is over." << std::endl;
+            } else if (burnRate > this->fuel) {
+                std::cout << "#Sorry, you don't have that much fuel." << std::endl;
+            } else {
+                //Calculate new application state
+                time = time + 1;
+                altitude = altitude - velocity;
+                velocity = ((velocity + GRAVITY) * 10 -
+                            burnRate * 2) / 10;
+                fuel = fuel - burnRate;
                 if (altitude <= 0) {
-                    std::cout << "#The game is over." << std::endl;
-                } else if (burnRate > fuel) {
-                    std::cout << "#Sorry, you don't have that much fuel." << std::endl;
-                } else {
-                    //Calculate new application state
-                    time = time + 1;
-                    altitude = altitude - velocity;
-                    velocity = ((velocity + GRAVITY) * 10 -
-                                burnRate * 2) / 10;
-                    fuel = fuel - burnRate;
-                    if (altitude <= 0) {
-                        altitude = 0;
-                        if (velocity <= 5) {
-                            std::cout << "#You have landed safely." << std::endl;
-                        } else {
-                            std::cout << "#You have crashed." << std::endl;
-                        }
+                    altitude = 0;
+                    if (velocity <= 5) {
+                        std::cout << "#You have landed safely." << std::endl;
+                    } else {
+                        std::cout << "#You have crashed." << std::endl;
                     }
                 }
-                //Print new values
-                this->printParam();
             }
         }
-    } while ((std::getline(std::cin, inputLine)) && (altitude > 0));
+        printParam();
+    }
+}
+
+int CalcNewValues::stringToInt(std::string str) {
+    for (int i = 0; i < str.length(); i++) {
+        if (!isdigit(str.at(i))) {
+            str.erase(i, 1);
+            i--;
+        }
+    }
+    return std::atoi(str.c_str());
 }
